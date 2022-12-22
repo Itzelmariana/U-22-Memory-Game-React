16075c601663f2371d767c306c3533bb2063dee7
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2022-12-21 22:43:57 -0600

    added files

---
+++ src/components/SingleCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SingleCard.css';
+
+export default function SingleCard({ cards }) {
+  return (
+    <div className='card-grid'>
+      {cards.map((card) => (
+        <div className='card' key={card.id}>
+          <img className='front' src={card.src} alt='card front' />
+          <img className='back' src='/img/cover.png' alt='card back' />
+        </div>
+      ))}
+    </div>
+  );
+}
