702596d1a988bcd7204d311468017db13b170a88
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2022-12-22 19:23:01 -0600

    added flipping cards and css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,104 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
   // suffle cards
   function shuffleCards() {
     const shuffleCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
-
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffleCards);
     setTurns(0);
   }
 
-  console.log(cards, turns);
+  function handleChoice(card) {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  // compare 2 slected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        console.log("It's a match");
+        resetTurn();
+      } else {
+        console.log('no match');
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // function handleChoice(clickedCard) {
+  //   if (choiceOne) {
+  //     if (clickedCard.src === choiceOne.src) {
+  //       setCards((prevCards) => {
+  //         return prevCards.map((card) => {
+  //           if (card.src === choiceOne.src) {
+  //             return { ...card, matched: true };
+  //           } else {
+  //             return card;
+  //           }
+  //         });
+  //       });
+  //       console.log('match');
+  //       console.log(cards);
+  //     } else {
+  //       console.log('no match');
+  //     }
+  //     resetTurn();
+  //   } else {
+  //     setChoiceOne(clickedCard);
+  //   }
+  // }
+
+  function resetTurn() {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  }
 
   return (
     <div className='App'>
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
-      <SingleCard cards={cards} />
+
       <div className='card-grid'>
         {cards.map((card) => (
-          <div className='card' key={card.id}>
-            <img className='front' src={card.src} alt='card front' />
-            <img className='back' src='/img/cover.png' alt='card back' />
-          </div>
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
